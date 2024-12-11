4652f5aaabb3f907ecb4fee4f681aa542590af21
Alfonso Morales <alfonsomoralesak@gmail.com>
2024-12-11 11:33:08 -0600

    mejora al script
    Corrección en manejo de cambios locales

--- updateGRP.sh
+++ updateGRP.sh
@@ -10,13 +10,19 @@ ssh -T git@github.com
 echo "Cambiando al directorio del proyecto /home/grp_app/MotorContable/..."
 
 # Navegar al directorio del proyecto
-cd /home/grp_app/MotorContable/
+cd /home/grp_app/MotorContable/ || { echo "Error: No se pudo acceder al directorio del proyecto."; exit 1; }
+
+# Descartar cambios locales
+echo "Descartando cambios locales..."
+git checkout -- .  # Descarta cambios en archivos rastreados
+git clean -f       # Elimina archivos sin seguimiento
+echo "Cambios locales descartados exitosamente."
 
 # Mensaje indicando que se actualizará el repositorio
 echo "Actualizando el repositorio local con los últimos cambios del repositorio remoto..."
 
 # Actualizar el repositorio local con los cambios remotos
-git pull origin main
+git pull origin main || { echo "Error: No se pudo actualizar el repositorio."; exit 1; }
 
 # Mensaje final
 echo "Actualización completada exitosamente."
